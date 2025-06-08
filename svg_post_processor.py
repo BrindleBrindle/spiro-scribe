9cea295c4c4df51cc61da337b7bc7354cc3ba094
Sunny Jim <24286855+BrindleBrindle@users.noreply.github.com>
2025-06-08 13:34:52 -0700

    Add circle and roulette parsing to SVG post processor

--- svg_post_processor.py
+++ svg_post_processor.py
@@ -1,3 +1,8 @@
+import numpy as np
+import math
+from fractions import Fraction
+
+
 class SVGPostProcessor:
     """
     Converts provided Circle and Roulette patterns into vector graphics (SVG format).
@@ -5,7 +10,6 @@ class SVGPostProcessor:
     def __init__(self):
         # Create an SVG document
         self.doc = SVGDocument(500, 500)  # SVG document
-        self.elements = []  # list of drawing elements
 
     def parse_circle_array(self):
         # TODO: Fill in code here.
@@ -13,7 +17,7 @@ class SVGPostProcessor:
 
     def parse_circle(self, circle_data, svg_data):
         """
-        Parse a circle dictionary into a corresponding SVG <path> element.
+        Parse a circle dictionary into a corresponding SVG <circle> element.
 
         Arguments:
             circle_data (dict): Dictionary of circle parameters (defined in mm).
@@ -45,135 +49,85 @@ class SVGPostProcessor:
         circle = SVGCircle(r=radius, cx=center_x, cy=center_y, stroke_color="black", stroke_width=1, fill="none")
         self.doc.add_element(circle)
 
-    # def parse_roulette(self, roulette_data, toolpath_data, origin_offset):
-    #     """
-    #     Parse a roulette dictionary into a series of G code commands.
-    #     Append the commands to the local G code program.
+    def parse_roulette(self, roulette_data, svg_data):
+        """
+        Parse a roulette dictionary into a corresponding SVG <path> element.
 
-    #     Produces:
-    #         1) Comment containing roulette parameters.
-    #         2) Comment containing cut pass number.
-    #         3) G01: Rapid Z to safe Z height.
-    #         4) G01: Rapid XY to XY start.
-    #         5) G01: Z plunge to cutting depth at Z feedrate.
-    #         6) G01: Move to successive XY points at XY feedrate until pattern is complete.
-    #         Repeat steps (2)-(6) for specified number of passes.
+        Arguments:
+            roulette_data (dict): Dictionary of roulette parameters (defined in mm).
+                                  example_data = {"type": "roulette",
+                                                  "R": 6.5,
+                                                  "r": 2.5,
+                                                  "s": 1,
+                                                  "d": 3.5,
+                                                  "display_res": 200}
+            svg_data (dict): Dictionary of SVG output parameters. TODO: Show example data here.
 
-    #     Arguments:
-    #         roulette_data (dict): Dictionary of roulette parameters (defined in mm).
-    #                               example_data = {"type": "roulette",
-    #                                               "R": 6.5,
-    #                                               "r": 2.5,
-    #                                               "s": 1,
-    #                                               "d": 3.5,
-    #                                               "display_res": 200}
-    #         toolpath_data (dict): Dictionary of machining parameters (defined in mm or in).
-    #                               example_data = {"units": "imperial",
-    #                                               "safe_z": 0.25,
-    #                                               "jog_feed_xyz": 8.0,
-    #                                               "cut_feed_xy": 2.0,
-    #                                               "cut_feed_z": 1.0,
-    #                                               "depth_per_pass": 0.02,
-    #                                               "num_passes": 1,
-    #                                               "cut_res": 200}
-    #         origin_offset (tuple): X and Y amounts (defined in mm) by which to translate
-    #                                pattern to account for origin location (dX, dY).
+        Returns:
+            None
+        """
 
-    #     Returns:
-    #         None
-    #     """
+        def compute_point(theta):
+            """Helper function to compute the (x, y) point for a given theta."""
+            factor = (R + s * r)
+            x = factor * np.cos(theta) - s * d * np.cos(theta * factor / r)
+            y = factor * np.sin(theta) - d * np.sin(theta * factor / r)
+            return x, y
 
-    #     def compute_point(theta):
-    #         """Helper function to compute the (x, y) point for a given theta."""
-    #         factor = (R + s * r)
-    #         x = factor * np.cos(theta) - s * d * np.cos(theta * factor / r)
-    #         y = factor * np.sin(theta) - d * np.sin(theta * factor / r)
-    #         return x, y
+        # Ensure the input is a roulette
+        if roulette_data.get("type") != "roulette":
+            raise ValueError("The input data is not a roulette.")
 
-    #     # Ensure the input is a roulette
-    #     if roulette_data.get("type") != "roulette":
-    #         raise ValueError("The input data is not a roulette.")
+        # Extract roulette parameters (defined in mm).
+        R = roulette_data["R"]
+        r = roulette_data["r"]
+        s = roulette_data["s"]
+        d = roulette_data["d"]
+        cut_res = 200  # TODO: replace
 
-    #     # Extract units.
-    #     units = toolpath_data['units']
+        # TODO: Extract SVG parameters.
+        # Add SVG parameters:
+        #   - stroke_color
+        #   - stroke_width
+        #   - fill
+        #   - roulette resolution
 
-    #     # Extract roulette parameters (defined in mm). Convert to inches if units not metric.
-    #     if units == "metric":
-    #         R = roulette_data["R"]
-    #         r = roulette_data["r"]
-    #         s = roulette_data["s"]
-    #         d = roulette_data["d"]
-    #     else:
-    #         R = roulette_data["R"] / 25.4
-    #         r = roulette_data["r"] / 25.4
-    #         s = roulette_data["s"]
-    #         d = roulette_data["d"] / 25.4
+        # Define R and r as fractions.
+        R = Fraction(R)
+        r = Fraction(r)
 
-    #     # Extract toolpath parameters (already in correct units).
-    #     safe_z = toolpath_data['safe_z']
-    #     jog_feed_xyz = toolpath_data['jog_feed_xyz']
-    #     cut_feed_xy = toolpath_data['cut_feed_xy']
-    #     cut_feed_z = toolpath_data['cut_feed_z']
-    #     depth_per_pass = toolpath_data['depth_per_pass']
-    #     num_passes = toolpath_data['num_passes']
-    #     cut_res = toolpath_data['cut_res']
+        # Compute the effective radius.
+        effective_R = R + s * r
 
-    #     # Extract origin offsets (defined in mm).
-    #     if units == "metric":
-    #         offset_x, offset_y = origin_offset
-    #     else:
-    #         offset_x = origin_offset(0) / 25.4
-    #         offset_y = origin_offset(1) / 25.4
+        # Compute the GCD of the numerators and denominators.
+        numerator_gcd = math.gcd(r.numerator, effective_R.numerator)
+        denominator_lcm = (r.denominator * effective_R.denominator) // math.gcd(r.denominator, effective_R.denominator)
 
-    #     # Add a comment for the roulette.
-    #     self.add_comment(f"Roulette with parameters: R={R}, r={r}, s={s}, d={d}, res={cut_res}")
+        # Simplify the GCD as a fraction.
+        gcd_fraction = Fraction(numerator_gcd, denominator_lcm)
 
-    #     # Define R and r as fractions.
-    #     R = Fraction(R)
-    #     r = Fraction(r)
+        # Calculate the number of turns needed to close the path.
+        n_turns = r / gcd_fraction
+        total_angle = n_turns * 2 * np.pi
+        thetas = np.linspace(0, total_angle, cut_res, endpoint=False)
 
-    #     # Compute the effective radius.
-    #     effective_R = R + s * r
+        # Compute the starting point.
+        start_x, start_y = compute_point(thetas[0])
 
-    #     # Compute the GCD of the numerators and denominators.
-    #     numerator_gcd = math.gcd(r.numerator, effective_R.numerator)
-    #     denominator_lcm = (r.denominator * effective_R.denominator) // math.gcd(r.denominator, effective_R.denominator)
+        # Create a new path element and move to the starting point.
+        path = SVGPath(stroke_color="blue", fill="none", stroke_width=2)
+        path.move_to(start_x, start_y)
 
-    #     # Simplify the GCD as a fraction.
-    #     gcd_fraction = Fraction(numerator_gcd, denominator_lcm)
+        # Move to the next XY location.
+        for theta in thetas[1:]:
+            next_x, next_y = compute_point(theta)
+            path.line_to(next_x, next_y)
 
-    #     # Calculate the number of turns needed to close the path.
-    #     n_turns = r / gcd_fraction
-    #     total_angle = n_turns * 2 * np.pi
-    #     thetas = np.linspace(0, total_angle, cut_res, endpoint=False)
+        # Close the pattern by moving back to the starting point.
+        path.close_path()
 
-    #     for p in range(1, num_passes + 1):
-    #         # Compute the starting point.
-    #         first_x, first_y = compute_point(thetas[0])
-    #         start_x, start_y = first_x, first_y
-    #         start_x_offset = start_x + offset_x  # to account for origin location
-    #         start_y_offset = start_y + offset_y  # to account for origin location
-
-    #         # Add a comment with the number of the current pass.
-    #         self.add_comment(f"====== Cut Pass {p} ======")
-
-    #         # Jog to starting XY at safe Z height.
-    #         self.move_linear(z=safe_z, feedrate=jog_feed_xyz, comment="rapid move to safe Z")
-    #         self.move_linear(x=start_x_offset, y=start_y_offset, feedrate=jog_feed_xyz,
-    #                          comment="rapid move to XY start")
-
-    #         # Plunge into material.
-    #         self.move_linear(z=-p*depth_per_pass, feedrate=cut_feed_z, comment="Z plunge")
-
-    #         # Move to the next XY location.
-    #         for theta in thetas[1:]:
-    #             next_x, next_y = compute_point(theta)
-    #             next_x_offset = next_x + offset_x
-    #             next_y_offset = next_y + offset_y
-    #             self.move_linear(x=next_x_offset, y=next_y_offset, feedrate=cut_feed_xy)
-
-    #         # Close the pattern by moving back to the starting point.
-    #         self.move_linear(x=start_x_offset, y=start_y_offset, feedrate=cut_feed_xy)
+        # Add the finished path to the list of elements in the document. 
+        self.doc.add_element(path)
 
 
 class SVGCircle:
@@ -336,7 +290,7 @@ if __name__ == "__main__":
     doc = SVGDocument(500, 500)
 
     # Create a path and incrementally add commands
-    path1 = SVGPath(stroke="blue", fill="none", stroke_width=2)
+    path1 = SVGPath(stroke_color="blue", stroke_width=2, fill="none")
     path1.move_to(10, 10)      # Absolute move to (10, 10)
     path1.line_to(100, 10)     # Absolute line to (100, 10)
     path1.vertical_to(100)     # Absolute vertical line to y=100
@@ -344,7 +298,7 @@ if __name__ == "__main__":
     path1.close_path()         # Close the path (Z)
 
     # Create another path
-    path2 = SVGPath(stroke="red", fill="none", stroke_width=1)
+    path2 = SVGPath(stroke_color="red", stroke_width=1, fill="none")
     path2.move_to(150, 150)    # Absolute move to (150, 150)
     path2.line_by(50, 0)       # Relative line by (50, 0)
     path2.vertical_by(50)      # Relative vertical line by 50
